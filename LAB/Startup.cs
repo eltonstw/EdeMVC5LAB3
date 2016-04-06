a9d05f63ec23b771950396d54756d4384960ea12
eltonstw <eltons@edetw.com>
2016-04-06 21:54:15 +0800

    Saved

---
+++ LAB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB.Startup))]
+namespace LAB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
